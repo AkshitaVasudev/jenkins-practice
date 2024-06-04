8534ce4b49fe15dcca019f26837257847bfb6c06
AkshitaVasudev <121186734+AkshitaVasudev@users.noreply.github.com>
2024-06-04 16:03:29 +0530

    Add files via upload

---
+++ patt.java
@@ -0,0 +1,13 @@
+public class patt{
+public static void main(String args[]){
+int n=Integer.parseInt(args[0]);
+int i,j;
+for (i=1;i<=n;i++)
+{
+for(j=1;j<=i;j++){
+System.out.print(j+"\t");
+}
+System.out.println();
+}
+}
+}
